b21b4c905fd00c2062eeccaadba427c578f7cb2c
AngadK89 <93425810+AngadK89@users.noreply.github.com>
2025-03-04 08:29:33 +0000

    feat: Added images of all balls available for each draw.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,31 +2,34 @@ import streamlit as st
 import pandas as pd
 from lottery import get_stats
 
-st.header("Hi! Come get your lottery numbers!")
-
 (draw, filtered_results, analysed_results) = get_stats()
 no_draw_today = not draw
 
 if no_draw_today:
     st.header("It's a Sunday, take the day off!")
+
 else:
     draw_name = draw.replace("-", " ").title()
+    st.header(f"Hi! Come get your {draw_name} lottery numbers!")
 
-st.text("")
+    # Render image of available numbers for draw.
+    st.image(f"images/{draw}.png")
 
-if st.button("Get lottery numbers!", disabled=no_draw_today):
-    df = pd.read_csv(analysed_results)
+    st.text("")
 
-    st.header(f"The most frequent balls for {draw_name} are: ")
-    df
+    if st.button("Get lottery numbers!", disabled=no_draw_today):
+        df = pd.read_csv(analysed_results)
 
-# Just some basic padding between buttons.
-st.markdown("###")
+        st.header(f"The most frequent balls are: ")
+        df
 
-if st.button("Give me all the data!", disabled=no_draw_today):
-    st.header(f"Here's all the {draw_name} data for you!")
-    filtered_df = pd.read_csv(filtered_results)
-    filtered_df
+    # Just some basic padding between buttons.
+    st.markdown("###")
+
+    if st.button("Give me all the data!", disabled=no_draw_today):
+        st.header(f"Here's all the data for you!")
+        filtered_df = pd.read_csv(filtered_results)
+        filtered_df
 
 
 
